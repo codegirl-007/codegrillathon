40b26c2e1b6957ce1a61bf258772af80d6176c03
Stephanie Gredell <s.raide@gmail.com>
2025-07-16 13:10:35 -0700

    displaying username on welcome page, creating git ignore

--- handlers/welcome.go
+++ handlers/welcome.go
@@ -1,11 +1,31 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/markbates/goth/gothic"
 )
 
+type PageData struct {
+	Username string
+}
+
 func (h *Handler) Welcome(w http.ResponseWriter, r *http.Request) {
-	err := h.Template.ExecuteTemplate(w, "welcome.html", nil)
+	session, err := gothic.Store.Get(r, "user-session")
+	if err != nil {
+		http.Error(w, "Error retrieving session for welcome page", http.StatusInternalServerError)
+	}
+	username, ok := session.Values["user_name"].(string)
+	if !ok {
+		http.Error(w, "Not authenticated", http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Printf("username: %s", username)
+	err = h.Template.ExecuteTemplate(w, "welcome.html", &PageData{
+		Username: username,
+	})
 	if err != nil {
 		http.Error(w, "Template rendering error", http.StatusInternalServerError)
 	}
